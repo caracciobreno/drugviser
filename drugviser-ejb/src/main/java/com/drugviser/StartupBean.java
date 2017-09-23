036ea6a8adbe4664769e0c5859a2a402479d9332
Breno Caraccio <breno.sherrer@icloud.com>
2017-09-23 19:35:37 -0300

    Initial files and architetures
    Initial commit, all project now are maven-managed and are able to
    compile and deploy.

---
+++ drugviser-ejb/src/main/java/com/drugviser/StartupBean.java
@@ -0,0 +1,40 @@
+package com.drugviser;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+import javax.annotation.PostConstruct;
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
+import org.apache.log4j.Logger;
+
+/**
+ * Application Startup Singleton Session Bean.
+ *
+ * @author caracciobreno
+ */
+@Startup
+@Singleton
+public class StartupBean {
+
+    /**
+     * Default Logger for this Bean.
+     */
+    private static final Logger LOGGER = Logger.getLogger(StartupBean.class);
+
+    /**
+     * Maintain the server up status.
+     */
+    private static AtomicBoolean INITIALIZED_SERVER_STATE = new AtomicBoolean();
+
+    /**
+     * Initializes everything up.
+     */
+    @PostConstruct
+    void init() {
+        if (INITIALIZED_SERVER_STATE.get()) {
+            throw new RuntimeException("Server already started.");
+        }
+
+        INITIALIZED_SERVER_STATE.set(true);
+        LOGGER.warn("Server started!");
+    }
+}
